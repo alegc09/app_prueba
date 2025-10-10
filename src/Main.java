f0d2e0390bedcafa75843170dbf37ba19ac3e6c6
Laptop_Alejandro <alejandrogtzcruz_09@gmail.com>
2025-10-09 21:57:17 -0600

    Creación del programa

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Bienvenido a Screen Match");
+        System.out.println("Película: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula =8.2;
+
+        double media = (8.2 + 6.0 + 9.0)/3;
+
+        System.out.println(media);
+    }
+}
