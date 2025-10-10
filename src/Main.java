7b35f95aff1050469602000213e7a6f94cb45cec
Alejandro <alejandrogtzcruz09@gmail.com>
2025-10-09 22:26:18 -0600

    Se agrega variable sinopsis y clasificacion

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,16 @@ public class Main {
         double notaDeLaPelicula =8.2;
 
         double media = (8.2 + 6.0 + 9.0)/3;
-
         System.out.println(media);
-        System.out.println("Hola");
+
+        String sinopsis = """
+                Matrix es una paradoja
+                La mejor película del fin del milenio
+                Fue lanzada en:
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media / 2);
+        System.out.println(clasificacion);
     }
 }
